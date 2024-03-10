f0c5358272c8e591e3b1201cbd12f69a7780013a
YevhenB0512 <evgenijboychuk@gmail.com>
2024-03-10 19:41:29 +0300

    added path for user_profile and user_delete

--- users/urls.py
+++ users/urls.py
@@ -1,5 +1,5 @@
 from django.urls import path
-from .views import user_login, user_logout, user_registration, user_profile, profile_edit
+from .views import user_login, user_logout, user_registration, user_profile, profile_edit, profile_delete
 
 app_name = 'users'
 
@@ -9,4 +9,6 @@ urlpatterns = [
     path('registration/', user_registration, name='registration'),
     path('profile/', user_profile, name='profile'),
     path('profile/edit', profile_edit, name='edit'),
+    path('profile/<username>', user_profile, name='user_profile'),
+    path('profile/delete/', profile_delete, name='delete'),
 ]
