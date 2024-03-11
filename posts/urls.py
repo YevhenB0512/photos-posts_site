4d6d8ef7e55a5ec8dedb4dd4b4445a3b144f7b13
YevhenB0512 <evgenijboychuk@gmail.com>
2024-03-11 23:15:53 +0300

    added path for likes

--- posts/urls.py
+++ posts/urls.py
@@ -1,6 +1,6 @@
 from django.urls import path
 from .views import (home, post_create, post_delete, post_edit, post_detail, sent_comment,
-                    delete_comment, sent_reply, delete_reply, like_post)
+                    delete_comment, sent_reply, delete_reply, like_post, like_comment, like_reply)
 
 app_name = 'posts'
 
@@ -9,11 +9,13 @@ urlpatterns = [
     path('category/<tag>/', home, name='by_category'),
     path('post/create/', post_create, name='create'),
     path('post/<pk>/', post_detail, name='detail'),
-    path('post/<pk>/like', like_post, name='like_post'),
+    path('post/<pk>/like/', like_post, name='like_post'),
+    path('comment/like/<pk>', like_comment, name='like_comment'),
+    path('reply/like/<pk>', like_reply, name='like_reply'),
     path('post/edit/<pk>/', post_edit, name='edit'),
     path('post/delete/<pk>/', post_delete, name='delete'),
-    path('sent-comment/<pk>', sent_comment, name='sent_comment'),
-    path('comment/delete/<pk>', delete_comment, name='delete_comment'),
-    path('sent-reply/<pk>', sent_reply, name='sent_reply'),
-    path('reply/delete/<pk>', delete_reply, name='delete_reply'),
+    path('sent-comment/<pk>/', sent_comment, name='sent_comment'),
+    path('comment/delete/<pk>/', delete_comment, name='delete_comment'),
+    path('sent-reply/<pk>/', sent_reply, name='sent_reply'),
+    path('reply/delete/<pk>/', delete_reply, name='delete_reply'),
 ]
